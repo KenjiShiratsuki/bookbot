465c3c3f20f5d05c389680d08b6a128ecec66a09
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-01-31 13:14:38 -0500

    Added word count to end of content print.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+        word_count = len(file_contents.split())
         print(file_contents)
+        print(f"Total Word Count: {word_count}")
 main()
