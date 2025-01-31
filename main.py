3cb9ab974ba1454cb65c8b5eda6480be3c914010
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-01-31 14:40:31 -0500

    Added sort_on and dict_conversion functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Counts characters, skipping non-alpha characters like spaces, periods, commas, etc.
 def count_char(string_in):
     string_in_lowered = string_in.lower()
     chars = {}
@@ -7,7 +8,16 @@ def count_char(string_in):
         elif char not in chars and char.isalpha() == True:
             chars[char] = 1
     return chars
-    
+#Converts the count_char dict into a list of dictionaries for further iteration, sorting, and output
+def dict_conversion(char_dictionary):
+    char_dict_list = []
+    for char, count in char_dictionary.items():
+        new_dict = {"char": char, "num": count}
+        char_dict_list.append(new_dict)
+    return char_dict_list
+#Sorts the list of dictionaries created above
+def sort_on(char_dict_list):
+    return char_dict_list["num"]    
 
 #Main function, opens book, counts words, prints word count first
 def main():
@@ -15,7 +25,9 @@ def main():
         file_contents = f.read()
         word_count = len(file_contents.split())
         char_dictionary = count_char(file_contents)
+        char_dict_list = dict_conversion(char_dictionary)
+        char_dict_list.sort(reverse=True, key=sort_on)
         print(f"Total Word Count: {word_count}")
-        print(char_dictionary)
+        print(char_dict_list)
 
 main()
