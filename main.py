b833a6d6a01d6b778b4786e453daa87ee55cfb14
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-04-03 21:31:22 -0400

    Updated for lesson changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_counter
+import sys
 #Counts characters, skipping non-alpha characters like spaces, periods, commas, etc.
 def count_char(string_in):
     string_in_lowered = string_in.lower()
@@ -21,15 +23,18 @@ def sort_on(char_dict_list):
 
 #Main function, opens book, counts words, prints word count first
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) <2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    with open(sys.argv[1]) as f:
         file_contents = f.read()
-        word_count = len(file_contents.split())
+        word_count = word_counter(file_contents)
         char_dictionary = count_char(file_contents)
         char_dict_list = dict_conversion(char_dictionary)
         char_dict_list.sort(reverse=True, key=sort_on)
         print("--- Begin report of books/frankenstein.txt ---")
         print(f"{word_count} words found in the document\nThe following is a list of each alphabetical letter used in the document, with capital letters converted to lower case.")
         for item in char_dict_list:
-            print(f"The letter '{item['char']}' was found {item['num']} times.")
+            print(f"{item['char']}: {item['num']}")
         print("--- End report ---")
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_counter(file_contents):
+    word_count = len(file_contents.split())
+    return word_count
